3e323451681d03327dcfc2c4d28ac41eb5c43293
Nivas <kammarinivas@gmail.com>
2026-01-21 18:40:10 +0530

    core

---
+++ core/class,object,constructor/variables.py
@@ -0,0 +1,8 @@
+class Student:
+    total_students=0
+    def __init__(self,name):
+        self.name=name
+        Student.total_students+=1
+        print(f"new student has been created.Total students={Student.total_students}")
+s1=Student("Nivas")
+s2=Student("nivas")
