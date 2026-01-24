0ae31c8434fe657c9bc73586bf0bb196e6467703
Nivas <kammarinivas@gmail.com>
2026-01-24 15:33:54 +0530

    prime

--- programming.py
+++ programming.py
@@ -49,16 +49,16 @@
 #         else:
 #             print(f"5*{i}",end="")
 #######################################
-a=float(input())
-b=float(input())
-c=0
-while(round(a,1)<=b):
-    c=c+1
-    if c>1:
-        print(",",end=" ")
-    print(f"{a:.1f}^2",end="")
-    a=a+0.2
-print(".")
+# a=float(input())
+# b=float(input())
+# c=0
+# while(round(a,1)<=b):
+#     c=c+1
+#     if c>1:
+#         print(",",end=" ")
+#     print(f"{a:.1f}^2",end="")
+#     a=a+0.2
+# print(".")
 
 
 
@@ -66,3 +66,27 @@ print(".")
 # 6.7888
 # 7.88889
 # 9.20000
+#############################################
+# def isprime(a):
+#     fc=0
+#     for i in range(1,a+1):
+#         if a%i==0:
+#             fc=fc+1
+#     if fc==2:
+#         print("prime")
+#     else:
+#         print("not a prime")
+# n=int(input())
+# isprime(n)
+####################boolean
+def isprime(a):
+    fc=0
+    for i in range(1,a+1):
+        if a%i==0:
+            fc=fc+1
+    return fc==2
+n=int(input())
+if isprime(n):
+    print("prime")
+
+
