6f259adb47c5f007a05c7ee2c2758ba36a6bf731
Nivas <kammarinivas@gmail.com>
2026-01-29 12:32:10 +0530

    palindrome

--- programming/prog practice.py
+++ programming/prog practice.py
@@ -16,12 +16,23 @@
 #         l=r
 #     a=a//10
 # print(l)
-# automorphic number #
-n=int(input())
-if n>0:
-    a=n*n
-    b=a%10
-if n==b:
-    print("automorphic number")
+# # automorphic number #
+# n=int(input())
+# if n>0:
+#     a=n*n
+#     b=a%10
+# if n==b:
+#     print("automorphic number")
+# else:
+#     print("not a automorphic number")
+###########################################
+a=212
+if a<=0:
+    print("invalid input")
 else:
-    print("not a automorphic number")
+    s=0
+    for i in str(a):
+        if int(i)%2==0:
+            s=s+int(i)
+    print(s)
+
