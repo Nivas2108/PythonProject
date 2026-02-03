802c7f1d384294ee19e9a268d1ab9fc857c4e545
Nivas <kammarinivas@gmail.com>
2026-02-03 17:33:40 +0530

    commit

---
+++ core/class,object,constructor/varisble2.py
@@ -0,0 +1,7 @@
+# Q1. Create a class Student that:
+# •	Keeps track of the total number of students created.
+# •	Determines whether a student passed or failed based on a shared passing mark.
+# •	Provides a method to curve marks by increasing everyone’s marks by a percentage.
+# •	Has a utility to convert marks (0–100) into letter grades (A, B, C, etc.).
+class Student:
+    total_students=0
