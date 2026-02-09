5f3a4bea7a94ad105cd19734f841a311a0d03ae5
Nivas <kammarinivas@gmail.com>
2026-02-09 19:35:01 +0530

    core

--- core.py
+++ core.py
@@ -1,56 +1,47 @@
-# Use functools.reduce() with a lambda to find the largest number from a given list Dynamically
-import functools
-l=[1,2,23,45,73,9]
-l2=functools.reduce(lambda x,y:x if x>y else y,l)
-print(l2)
-#
-nums = [1, 2, 3]
-result = map(lambda x: x * 2, nums)
-nums.clear()
-print(list(result))
-###########################
+class LibraryMember:
+    borrowing_limit=3
+    def __init__(self,member_name,books_borrowed):
+        self.member_name=member_name
+        self.books_borrowed=books_borrowed
+    def borrowing(self):
+        if self.books_borrowed<LibraryMember.borrowing_limit:
+            self.books_borrowed += 1
+        else:
+            print("out of limit")
+    @classmethod
+    def update(cls,new_limit):
+        cls.borrowing_limit=new_limit
+    @staticmethod
+    def books(a):
+        if a.books_borrowed>1:
+            print("positive")
+        else:
+            print("negative")
+x1=LibraryMember("nivas",1)
+x2=LibraryMember("madhu",4)
+LibraryMember.borrowing(x1)
+LibraryMember.borrowing(x2)
+#####################
 from functools import reduce
-lists = [[1], [2, 3], [4]]
-result = reduce(lambda a, b: a + b, lists)
-print("Flattened:", result)
-#############################
-n=3.25
-p=int(n*100)
-print(f"{p}CM")
-#########################
-F=212
-C=int(((F-32)*5)/9)
-print(f"{C}F")
-#############################
-a,b,c=3,4,7
-if a<b and a<c:
-    print(a)
-if b<a and b<c:
-    print(b)
-if c<a and c<b:
-    print(c)
-################################
-n=499
-if 50<=n>=500:
-    print("invalid number")
-else:
-    if n%2==0:
-        print(n%5)
-    else:
-        print(n%7)
-##########################################
-import functools
-s="*@hi123"
-n= lambda text: sum(map(ord, filter(str.isalpha,text)))
-result = n
-print(result)
-#############################################
-# l="hi123@"
-# l2=reduce(lambda x,y:x+ord(y,list(lambda x:x.isalpha(),l)),0)
-# print()
-##########################################
-l1=[1,2,3,34,4,455,6,6,6,7,8,8,8,9,10]
-l=set(l)
-print(l)
+l=[5,10,15,20,25]
+print(reduce(lambda a,b:a+b,filter(lambda x:x%5==0,map(lambda x:x**2,l))))
+############
+# class movie:
+#     total_movies=0
+#     def __init__(self,name,director):
+#         self.name=name
+#         self.director=director
+#         movie.total_movies+=1
+#     def cinema(self,t):
+#         if len(self.name)<4:
+#             print("alteast 4")
+#         else:
+#             print("valid")
+#     @classmethod
+#
+# m1=movie("sye","rajamouli")
+# m2=movie("spirit","vanga")
+
+
 
 
