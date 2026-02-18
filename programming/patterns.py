e48a812a966c6166667acc7d009eee76a587d740
Nivas <kammarinivas@gmail.com>
2026-02-18 18:30:34 +0530

    patterns

--- programming/patterns.py
+++ programming/patterns.py
@@ -76,13 +76,37 @@
 #             print(end="0 ")
 #     print()
 ############## OPPOSITE DIAGONAL ,ROWS ,COLUMNS ######
-n=5
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i+j==n+1:
+#             print(end="1 ")
+#         elif i+j<n+1:
+#             print(end="2 ")
+#         else:
+#             print(end="3 ")
+#     print()
+################ i decrement j decrement #############
+# n=5
+# for i in range(n,0,-1):
+#     for j in range(n,0,-1):
+#         if i<=j:
+#             print(j,end=" ")
+#         else:
+#             print(i,end=" ")
+#     print()
+########## $$$ different pattern module basic $$$ #####
+n=6
+c=1
 for i in range(1,n+1):
     for j in range(1,n+1):
-        if i+j==n+1:
-            print(end="1 ")
-        elif i+j<n+1:
-            print(end="2 ")
+        print(c,end=" ")
+        if c==1:
+            c=0
         else:
-            print(end="3 ")
+            c=1
     print()
+    if c==1:
+        c=0
+    else:
+        c=1
