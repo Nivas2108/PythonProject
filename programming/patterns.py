71535c8545c6f886cc8aa9707a553c3900115543
Nivas <kammarinivas@gmail.com>
2026-02-20 19:29:12 +0530

    patterns triangle

--- programming/patterns.py
+++ programming/patterns.py
@@ -96,17 +96,90 @@
 #             print(i,end=" ")
 #     print()
 ########## $$$ different pattern module basic $$$ #####
-n=6
-c=1
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(c,end=" ")
-        if c==1:
-            c=0
-        else:
-            c=1
-    print()
-    if c==1:
-        c=0
-    else:
-        c=1
+# n=6
+# c=1
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(c,end=" ")
+#         if c==1:
+#             c=0
+#         else:
+#             c=1
+#     print()
+#     if c==1:
+#         c=0
+#     else:
+#         c=1
+################# left down triangle pattern ##################
+# n=5
+# for i in range(1,n+1):
+#     print("*"*i)
+################# right down triangle pattern #############
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,n-i+1):
+#         print(end=" ")##print(" "*(n-i),end="")
+#     for j in range(1,i+1):
+#         print(end="*")##print("*"*i)
+#     print()
+################# left upper triangle pattern ##############
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,n-i+2):
+#         print(end="*")##print(" "*(n-i+1),end="")
+#     print()
+################# right upper triangle #################
+# n=5
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(end=" ")
+#     for j in range(1,n-i+2):
+#         print(end="*")
+#     print()
+################ Alphabets triangle ##############
+# n=5
+# c=65
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(chr(c),end=" ")
+#         c=c+1
+#     print()
+#################### Even left down triangle #####
+# n=5
+# c=2
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(c,end=" ")
+#         c+=2
+#     print()
+############ one line even and other odd left down ######
+# n=5
+# c=2
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(c,end=" ")
+#         c+=2
+#     print()
+#     c-=1
+########### print sum of number is even or odd in pattern ###
+# n=5
+# c=1
+# for i in range(1,n+1):
+#     sum=0
+#     for j in range(1,i+1):
+#         print(c,end=" ")
+#         sum+=c
+#         c+=1
+#     print(f"-{sum} @",end=" ")
+#     if sum%2==1:
+#         print("odd")
+#     else:
+#         print("even")
+############### one line straight one line reverse ######
+# n=6
+# c=1
+# for i in range(1,n+1):
+#     if i%2==1:
+#         print(*range(1,n-i+2))
+#     else:
+#         print(*range(n-i+1,0,-1))
