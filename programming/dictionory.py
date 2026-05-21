f790313e0b2b065109a6d7228a5a26fd8521a2f2
Nivas <kammarinivas@gmail.com>
2026-05-21 19:12:47 +0530

    dictionary

---
+++ programming/dictionory.py
@@ -0,0 +1,42 @@
+## dictionary is a ordered collection ##
+# d=dict()
+# d["name"]="nivas"
+# d["age"]="21"
+# d["salary"]="23456789"
+# d["age"]="24"
+# d1={}
+# d1["sal"]="123456"
+# d1["dfg"]="wesrdf"
+# d.update(d1)
+# d1.update(d)
+# print(d1)
+# print(d)
+# print(d["age"])
+# print(d.get("age"))
+# print(d.get("ages"))
+# print(d.get("names","chintu"))
+# d.pop("age")
+# d.popitem()
+# print(d)
+# print(d.items())
+# for i in d.items():
+#     print(i[0],i[1])
+#### frequency ########
+# l=[10,20,304,10,20,30,30,40]
+# d={}
+# for i in range(0,len(l)):
+#     if l[i] in d:
+#         d[l[i]]=d[l[i]]+1
+#     else:
+#         d[l[i]]=1
+# print(d)
+###### frequency short #####
+# l=[10,20,304,10,20,30,30,40]
+# d={}
+# for i in range(0,len(l)):
+#     d[l[i]]=d.get(l[i],0)+1
+# print(d)
+#####
+
+
+
